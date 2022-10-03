9fe379e59d1215513ddb5e3127900e54a88d203a
msiddique035 <108491114+msiddique035@users.noreply.github.com>
2022-10-03 10:03:46 -0500

    kafka consumer

---
+++ src/main/java/com/example/consumer/service/JsonKafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.example.consumer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(String event){
+        LOGGER.info(String.format("Json message recieved -> %s", event));
+    }
+}
